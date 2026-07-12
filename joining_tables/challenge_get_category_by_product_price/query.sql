02ac61f99c8cd8628c261e284a655f8b93f5e65b
sharmarahulmait-pixel <>
2026-07-12 20:21:35 +0000

    Challenge: Get Category by Product Price

--- joining_tables/challenge_get_category_by_product_price/query.sql
+++ joining_tables/challenge_get_category_by_product_price/query.sql
@@ -1,2 +1,6 @@
-SELECT Distinct category_name , 
-from product
+SELECT DISTINCT category.name
+FROM product
+JOIN category 
+ON product.category_id = category.id
+Where product.price > 450 
+GROUP BY category.name
