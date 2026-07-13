a5de18b4479ed8ada73170ec7d221643a5320fdd
sharmarahulmait-pixel <>
2026-07-13 19:08:36 +0000

    Challenge: FULL JOIN Usage

---
+++ joining_tables/challenge_full_join_usage/query.sql
@@ -0,0 +1,10 @@
+SELECT
+courses.course_id,
+courses.course_name,
+courses.description,
+enrollments.student_name,
+enrollments.enrollment_date
+From courses
+FULL JOIN enrollments
+ON courses.course_id = enrollments.course_id
+order by courses.course_id
